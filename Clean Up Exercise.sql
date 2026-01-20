a834333f9798176c9ae4a61c4d5cc58bdbc20f6d
wilfex <wilfex81@gmail.com>
2026-01-20 02:49:45 -0500

    feat: completed standadization, Null values, and removed unecessary comlumns from the data

--- Clean Up Exercise.sql
+++ Clean Up Exercise.sql
@@ -118,6 +118,133 @@ FROM
 
 
 
+-- Standardizing data
+SELECT  company, TRIM(company)
+FROM
+    layoffs_staging2
+;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE layoffs_staging2
+SET company =  TRIM(company);
+
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT  company, TRIM(company)
+FROM
+    layoffs_staging2
+;
+
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = 'United States'
+WHERE country LIKE 'United States%';
+
+
+SELECT `date`
+FROM layoffs_staging2;
+
+
+UPDATE layoffs_staging2
+SET `date` = str_to_date(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+SELECT *
+FROM layoffs_staging2;
+
+
+-- NULL Values
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off is NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+UPDATE layoffs_staging2
+SET industry  = NULL
+WHERE industry = '';
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Bally%';
+
+SELECT * 
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+SELECT * 
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off is NULL;
+
+
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off is NULL;
+
+
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+
+
+
+
+
+
+
+
+-- 1. Remove Duplicates - Done
+-- 2. Standardise the data - Done
+-- 3. Null Values or blank values - Done
+-- 4. Remove any column and rows that are not necessary(depends) - Done
+
+
+
 
 
 
